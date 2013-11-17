b327f4576cdf615b824be1fcc4bb7a491630ae9e
cleentfaar <leentfaar87@gmail.com>
2013-11-17 13:35:13 +0100

    removed (accidentally) added testing reports

--- src/Cleentfaar/Cilex/Tests/Provider/FilesystemServiceProviderTest.php
+++ src/Cleentfaar/Cilex/Tests/Provider/FilesystemServiceProviderTest.php
@@ -1,6 +1,15 @@
 <?php
 
-namespace Cleentfaar\Cilex\Provider\Tests;
+/*
+ * This file is part of Filesystem Service Provider.
+ *
+ * (c) 2013 Cas Leentfaar <info@casleentfaar.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cleentfaar\Cilex\Tests\Provider;
 
 use Cleentfaar\Cilex\Provider\FilesystemServiceProvider;
 use Cilex\Application;
